8365ead0288cd45beb427f4b2c7035ca83f71a9e
MoreStrings <aayushparajuli22222@gmail.com>
2025-01-19 09:23:23 +0545

    calculator

--- sum.js
+++ sum.js
@@ -1,3 +1,22 @@
-var x = 10
+let a = 10
+let b = 5
 
-console.log(x)
+let operator = "-"
+
+switch(operator){
+    case "+":
+        console.log(a+b)
+        break
+    case "-":
+        console.log(a-b)
+        break
+    case "*":
+        console.log(a*b)
+        break
+    case "/":
+        console.log(a/b)
+        break
+    default:
+        console.log("invalid")
+        break
+}
