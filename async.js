5b50b8da0a472975e2aa636721161c07e208d143
MoreStrings <aayushparajuli22222@gmail.com>
2025-01-21 08:53:24 +0545

    async/await example

--- async.js
+++ async.js
@@ -1,6 +1,30 @@
-console.log("First")
-setTimeout(() =>{
-    console.log("Time out completed")
-    }, 3000
-) 
-console.log("last")
+//console.log("First")
+//setTimeout(() =>{
+//    console.log("Time out completed")
+//    }, 3000
+//) 
+//console.log("last")
+
+//  async/await
+
+function fetchData(){
+    return new Promise((resolve, reject) => {
+        setTimeout( () => {
+            const success = true
+            if (success){
+                resolve("Data fetched")
+            }else{
+                reject("Error fetching data")
+            }
+        },2000)
+    })
+}
+
+async function displayData(){
+    console.log("initial")
+    const data = await fetchData();
+    console.log(data);
+    console.log("final")
+}
+
+displayData()
